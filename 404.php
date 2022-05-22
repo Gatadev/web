a1b9543fb4a992f2b9f76f48ce33bb82c12555f7
gatadev <kairateadoro@gmail.com>
2022-05-22 00:52:21 -0500

    404 pagina

---
+++ 404.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<head>
+
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>oops</title>
+	<link rel="stylesheet" href="css/estilos.css">
+	<link rel="icon" href="imgs/html.png">
+
+</head>
+
+
+<html>
+
+<body>
+    
+<p>Nothing</p>
+
+</body>
+
+
+
+
+</html>
