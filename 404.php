1a761382b6f4f88780149282d8731520092b76b4
gatadev <kairateadoro@gmail.com>
2022-05-22 13:01:38 -0500

    404

--- 404.php
+++ 404.php
@@ -5,8 +5,7 @@
     <meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>oops</title>
-	<link rel="stylesheet" href="css/estilos.css">
-	<link rel="icon" href="imgs/html.png">
+	
 
 </head>
 
@@ -15,7 +14,7 @@
 
 <body>
     
-<p>Nothing</p>
+<p>Nothing :b</p>
 
 </body>
 
