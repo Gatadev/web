baad87ce8b0847bf978be8654f984e738dfb242f
gatadev <kairateadoro@gmail.com>
2022-05-25 21:18:51 -0500

    agregue el formulario

--- js/main.js
+++ js/main.js
@@ -21,3 +21,17 @@ btnSwitch.addEventListener('click', () => {
 document.body.classList.toggle('dark');
 btnSwitch.classList.toggle('active');
 });
+
+
+function copiarAlPortapapeles() {
+
+  var aux = document.createElement("input");
+  aux.setAttribute("value", "Gata#4494");
+  document.body.appendChild(aux);
+  aux.select();
+  document.execCommand("copy");
+  document.body.removeChild(aux);
+
+  alert("Tag copiada al portapapeles.");
+
+}
